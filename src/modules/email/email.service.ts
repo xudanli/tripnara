f263faae735ac5046d8bd2677487aa308004e99a
xudanli <2293028143@qq.com>
2025-11-27 14:45:03 +0000

    fix: 修复 nodemailer 导入方式以兼容 TypeScript

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as nodemailer from 'nodemailer';
-import { Transporter } from 'nodemailer';
+import nodemailer from 'nodemailer';
+import type { Transporter } from 'nodemailer';
 
 export interface SendEmailOptions {
   to: string;
